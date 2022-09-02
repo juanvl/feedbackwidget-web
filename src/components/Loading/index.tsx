7b0c426c4405ad2a7339b82778f948aab6f9d429
Juan Victor Leite <jnvleite@gmail.com>
2022-09-02 16:10:50 -0300

    chore: add tailwind prettier plugin

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,8 +2,8 @@ import { CircleNotch } from 'phosphor-react';
 
 export function Loading() {
   return (
-    <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
-      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
+    <div className="flex h-6 w-6 items-center justify-center overflow-hidden">
+      <CircleNotch weight="bold" className="h-4 w-4 animate-spin" />
     </div>
   );
 }
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -9,10 +9,10 @@ export function Widget() {
         <WidgetForm />
       </Popover.Panel>
 
-      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-        <ChatTeardropDots className="w-6 h-6" />
+      <Popover.Button className="group flex h-12 items-center rounded-full bg-brand-500 px-3 text-white">
+        <ChatTeardropDots className="h-6 w-6" />
 
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+        <span className="max-w-0 overflow-hidden transition-all duration-500 ease-linear group-hover:max-w-xs">
           <span className="pl-2" />
           Feedback
         </span>
